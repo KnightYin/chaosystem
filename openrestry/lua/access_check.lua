99ac9c8a0b72575183295134c03189bce09e0922
Haoyu Yin <Mr.YinKnight@outlook.com>
2020-10-08 16:23:59 +0800

    register and sign in.

---
+++ openrestry/lua/access_check.lua
@@ -0,0 +1,26 @@
+local json = require "cjson.safe"
+local json_decode = json.decode
+
+ngx.req.read_body()
+local data = ngx.req.get_body_data()
+
+local json_data = json_decode(data)
+
+if not json_data then
+    ngx.exit(ngx.HTTP_BAD_REQUEST)
+end
+
+if ngx.var.path == "register" or ngx.var.path == "signin" then
+    return
+end
+
+
+local user_id = json_data["user_id"]
+local tokens = ngx.shared.tokens
+local token, _ = tokens:get(user_id)
+if token == nil or token ~= json_data["token"] then
+    ngx.exit(ngx.HTTP_UNAUTHORIZED)
+end
+
+--- 后续使用
+ngx.ctx.json_data = json_data
---
+++ openrestry/lua/register.lua
@@ -0,0 +1,30 @@
+local json_encode = require "cjson.safe.encode"
+
+local users_table = ngx.shared.users
+local json_data = ngx.ctx.json_data
+
+local user_id = json_data["user_id"]
+local password = json_data["password"]
+
+if user_id == nil or password == nil then
+    ngx.exit(ngx.HTTP_BAD_REQUEST)
+end
+
+local user_info, _ = users_table:get(user_id)
+
+if user_info ~= nil then
+    ngx.say(json_encode({code=400, msg="用户名已经注册."}))
+    return
+end
+
+local salt = "abcd"
+
+local succ, err, forcible = users_table:set(user_id, {salt=salt, password=ngx.md5(password .. salt)})
+
+if succ then
+    ngx.say(json_encode({code=200, msg="注册成功."}))
+    return
+else
+    ngx.say(json_encode({code=500, msg="注册失败." .. err}))
+    return
+end
